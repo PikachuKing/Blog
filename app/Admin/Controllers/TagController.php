d26fea4137b4c1aae6aa14934923790bfee3ec72
PikachuKing <pikachuking01@gmail.com>
2019-08-25 23:36:27 +0800

    完成标签管理

---
+++ app/Admin/Controllers/TagController.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\Tag;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
+
+class TagController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'App\Models\Tag';
+
+    /**
+     * 标签管理列表页
+     * @param Content $content
+     * @return Content
+     */
+    public function index(Content $content)
+    {
+        return $content
+            ->header('标签')
+            ->description('列表')
+            ->body($this->grid());
+    }
+
+    /**
+     * 添加页
+     * @param Content $content
+     * @return Content
+     */
+    public function create(Content $content)
+    {
+        return $content
+            ->header('添加标签')
+            ->body($this->form());
+    }
+
+    /**
+     * 编辑页
+     * @param $id
+     * @param Content $content
+     * @return Content
+     */
+    public function edit($id, Content $content)
+    {
+        return $content
+            ->header('修改标签')
+            ->body($this->form()->edit($id));
+    }
+
+    /**
+     * 详情页
+     * @param $id
+     * @param Content $content
+     * @return Content
+     */
+    public function show($id, Content $content)
+    {
+        return $content
+            ->header('标签详情')
+            ->body($this->detail($id));
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Tag);
+
+        $grid->column('name', '标签名称');
+        $grid->column('article_num', '标签文章数')->sortable();
+        $grid->column('search_num', '标签热度')->sortable();
+        $grid->column('created_at', '创建时间');
+        $grid->column('updated_at', '更新时间');
+        $grid->disableExport();
+
+        // 添加标签名称过滤
+        $grid->filter(function($filter){
+            // 去掉默认的id过滤器
+            $filter->disableIdFilter();
+            // 添加标签名称字段过滤器
+            $filter->like('name', '标签名称');
+        });
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Tag::findOrFail($id));
+
+        $show->field('name', '标签名称');
+        $show->field('article_num', '标签文章数');
+        $show->field('search_num', '标签热度');
+        $show->field('created_at', '创建时间');
+        $show->field('updated_at', '更新时间');
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Tag);
+
+        $form->text('name', '标签名称');
+
+        return $form;
+    }
+}
---
+++ database/migrations/2019_08_25_212256_create_tags_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->unique()->comment('标签名称');
+            $table->integer('article_num')->default(0)->comment('对应的文章数');
+            $table->integer('search_num')->default(0)->comment('搜索热度');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+    }
+}
