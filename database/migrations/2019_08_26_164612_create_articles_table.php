4220e1c12bb5f880871bb5dfc49c1ffc19341ec5
pikachuking <pikachuking@qq.com>
2019-08-26 09:52:11 +0000

    articles_migration

---
+++ database/migrations/2019_08_26_164612_create_articles_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('user_id')->comment('用户ID');
+            $table->foreign('user_id')->references('id')->on('admin_users');
+            $table->unsignedBigInteger('category_id')->comment('分类ID');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->string('title')->nullable(false)->comment('标题');
+            $table->string('slug')->unique();
+            $table->string('description')->nullable()->comment('摘要');
+            $table->text('content')->nullable(false)->comment('内容');
+            $table->boolean('is_draft')->default(false)->comment('是否是草稿');
+            $table->integer('view_number')->default(0)->comment('阅读数');
+            $table->timestamp('published_at')->nullable()->comment('发布时间');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
